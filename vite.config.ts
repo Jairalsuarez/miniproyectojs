64133c1325c796a0042bdc817359036d3906866b
Jaira Suarez <jairsuarez9806@gmail.com>
2026-02-24 22:40:57 -0500

    fix: regenerando permisos de binarios

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,16 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = resolve(__filename, '..');
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  build: {
+  build:{
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         bienvenida: resolve(__dirname, 'pages/bienvenida.html')
       }
-    },
-    outDir: 'dist',
+    }
   }
 })
